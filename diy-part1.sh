5723b9e667ef00769c757fbbd2eaed9a22a8f70f
Kameronski <kamsim@gmail.com>
2020-12-02 23:16:10 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,6 +16,7 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
-./openwrt/scripts/feeds update -a
-./openwrt/scripts/feeds install -a
+./scripts/feeds update -a
+./scripts/feeds install -a
